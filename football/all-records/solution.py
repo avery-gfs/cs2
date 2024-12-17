88c13587de3c21e696c97d4976f335e5127d26c0
Avery <anortonsmith@germantownfriends.org>
2024-12-17 12:56:06 -0500

    comments

--- football/all-records/solution.py
+++ football/all-records/solution.py
@@ -9,10 +9,13 @@ for game in games:
     game["away_score"] = int(game["away_score"])
     game["home_score"] = int(game["home_score"])
 
+# wins and losses are dictionaries for keeping track of
+# stats for every team, where keys are team names and
+# values are win or loss counts
 wins = {}
 losses = {}
 
-# Initialize wins and losses to zero for each time
+# Initialize wins and losses to zero for each team
 for game in games:
     homeTeam = game["home_team"]
     awayTeam = game["away_team"]
