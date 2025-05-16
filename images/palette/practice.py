bdfa66be08dacb8eba4bcd6ed30473f4e64f18ab
Avery <anortonsmith@germantownfriends.org>
2025-05-16 08:59:59 -0400

    instructions

--- images/palette/practice.py
+++ images/palette/practice.py
@@ -18,23 +18,40 @@ palette = [
 ]
 
 # Calculate color distance between two colors
-# https://en.wikipedia.org/wiki/Color_difference
 
 def distance(c1, c2):
 	(r1, g1, b1) = c1
 	(r2, g2, b2) = c2
+
+	# See https://en.wikipedia.org/wiki/Color_difference
+
 	return # Your code goes here!
 
 for y in range(im.height):
 	for x in range(im.width):
 		imgColor = im.getpixel((x, y))
 
-		minColor = None
+		# Look through the colors in palette, and find
+		# the one that is closest to the current pixel color
+
+		# Use this closest palette color in the output image
+
+		bestColor = None
 		minDist = None
 
 		# Your code goes here!
 
-		output.putpixel((x, y), minColor)
+		# Loop through each color in the color palette
+
+		# For each palette color, calculate the distance between
+		# the current pixel color and the palette color. If the distance
+		# is smaller than the current smallest distance (minDist), update
+		# minDist and bestColor accordingly.
+
+		# Once we've found the palette color which matches out pixel color
+		# most closely, we use this closest color in out output image
+
+		output.putpixel((x, y), bestColor)
 
 # Save output image
 output.save("palette.png")
